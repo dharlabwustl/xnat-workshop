1769310462c3c230b8f93eb90a052d992a188199
Autumn Xu <autumn@wustl.edu>
2023-01-31 15:13:34 -0600

    tryng

--- src/main/java/org/nrg/xnat/workshop/plugin/WorkshopXnatPlugin.java
+++ src/main/java/org/nrg/xnat/workshop/plugin/WorkshopXnatPlugin.java
@@ -18,8 +18,8 @@ import org.springframework.context.annotation.ComponentScan;
 
 @XnatPlugin(value = "workshopPlugin", name = "XNAT 1.7 Workshop 2016 Plugin", entityPackages = "org.nrg.xnat.workshop.entities",
             dataModels = {@XnatDataModel(value = WorkshopBiosamplecollectionBean.SCHEMA_ELEMENT_NAME,
-                                         singular = "Biosample Collection",
-                                         plural = "Biosample Collections"),
+                                         singular = "Stroke Edema Collection",
+                                         plural = "Stroke Edema Collection"),
                           @XnatDataModel(value = RadRadiologyreaddataBean.SCHEMA_ELEMENT_NAME,
                                          singular = "Radiology Read",
                                          plural = "Radiology Reads")})
