f854b6c227bc08916dc3aea1b0c8933c3ce9e7ad
Autumn Xu <autumn@wustl.edu>
2023-06-07 20:40:53 -0500

    this version should upload

--- src/main/java/org/nrg/xnat/workshop/plugin/WorkshopXnatPlugin.java
+++ src/main/java/org/nrg/xnat/workshop/plugin/WorkshopXnatPlugin.java
@@ -11,15 +11,14 @@ package org.nrg.xnat.workshop.plugin;
 
 import org.nrg.framework.annotations.XnatDataModel;
 import org.nrg.framework.annotations.XnatPlugin;
-//import org.nrg.xdat.bean.WorkshopEdemamarkerBean;
-import org.nrg.xdat.bean.RadRadiologyreaddataBean;
 import org.nrg.xdat.bean.WorkshopBiosamplecollectionBean;
+import org.nrg.xdat.bean.RadRadiologyreaddataBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-@XnatPlugin(value = "workshopPlugin", name = "Stroke Edema Plugin modified from XNAT 1.7 Workshop 2016 Plugin", entityPackages = "org.nrg.xnat.workshop.entities",
+@XnatPlugin(value = "workshopPlugin", name = "XNAT 1.7 Workshop 2016 Plugin", entityPackages = "org.nrg.xnat.workshop.entities",
             dataModels = {@XnatDataModel(value = WorkshopBiosamplecollectionBean.SCHEMA_ELEMENT_NAME,
-                                         singular = "Stroke Edema Collection",
+                                         singular = "Stroke Edema Collection", // Biosample Collection
                                          plural = "Stroke Edema Collections"),
                           @XnatDataModel(value = RadRadiologyreaddataBean.SCHEMA_ELEMENT_NAME,
                                          singular = "Radiology Read",
